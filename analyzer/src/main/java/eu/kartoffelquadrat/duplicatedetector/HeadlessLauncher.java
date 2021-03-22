8ef90cf4fc7fcee0d34963f31da7c5e4fd8f88fb
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2021-03-22 10:21:08 -0400

    Set up maven minimal requirements

---
+++ analyzer/src/main/java/eu/kartoffelquadrat/duplicatedetector/HeadlessLauncher.java
@@ -0,0 +1,8 @@
+package eu.kartoffelquadrat.duplicatedetector;
+
+public class HeadlessLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Headless Launcher starting...");
+    }
+}
